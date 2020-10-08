d0787a915605be15b0418dd528d1b4b1e16cbce3
MyBhive <delphine.miel0318@gmail.com>
2020-10-08 15:54:36 +0300

    update

--- controller.py
+++ controller.py
@@ -12,25 +12,21 @@ Creating controller's class to manage the game
 
 class Controller:
     def __init__(self):
-        super(labyrinthe).__init__()
-        super(view).__init__()
-        super(characters).__init__()
-        super(item).__init__()
-        super(characters).__init__()
         self.map_lab = labyrinthe.Labyrinthe("laby.txt")
         # items: 'E' for Ether / 'N' for Needle / 'P' for Pipe
         self.items = [item.Items("E", 0, 0),
                       item.Items("N", 0, 0),
-                      item.Items("P", 0, 0)] #quels positier instancier???
+                      item.Items("P", 0, 0)]  #quels positier instancier???
         # looking for paths to set the items
         self.paths = labyrinthe.Labyrinthe.show_list(".")
         # random position for item
+
     def set_items(self):
         for item in self.items:
             item = choice(self.items)
             position = choice(self.paths)
             item = position
-        #c'est nul je sais! j'y travaille
+        # c'est nul je sais! j'y travaille
 
 
     def run_the_game(self):
@@ -38,6 +34,6 @@ class Controller:
             labyrinthe.Labyrinthe.show_list()
             characters.McGyver.position()
             # if mcgyver position == guardien position:
-                #if item == 3 donc gagné
+            # if item == 3 donc gagné
             # if item != 3 donc perdu
             # input rejouer ou quitter et relancer le jeu au zéro.
